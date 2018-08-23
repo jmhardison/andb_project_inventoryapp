5eb6b9e9170ed1fb74eea2107af4c2634caec72e
Jonathan Hardison <jmh@jonathanhardison.com>
2018-08-22 22:57:28 -0500

    stubbing in sale button

--- app/src/main/java/com/jonathanhardison/andb_project_inventoryapp/InventoryCursorAdapter.java
+++ app/src/main/java/com/jonathanhardison/andb_project_inventoryapp/InventoryCursorAdapter.java
@@ -5,8 +5,10 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jonathanhardison.andb_project_inventoryapp.data.InventoryContract;
 
@@ -32,6 +34,7 @@ public class InventoryCursorAdapter extends CursorAdapter {
         TextView prodNameView = view.findViewById(R.id.name);
         TextView prodPriceView = view.findViewById(R.id.price);
         TextView prodQuantityView = view.findViewById(R.id.quantity);
+        Button saleButton = view.findViewById(R.id.buttonSale);
 
         //get index of columns
         int prodNameIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME);
@@ -40,13 +43,23 @@ public class InventoryCursorAdapter extends CursorAdapter {
 
         //get real value
         String prodNameText = cursor.getString(prodNameIndex);
-        String prodPriceText = "$" + String.format("%.2f", cursor.getDouble(prodPriceIndex));
-        String prodQuantityText = String.valueOf(cursor.getInt(prodQuantityIndex));
+        String prodPriceText = "Price: $" + String.format("%.2f", cursor.getDouble(prodPriceIndex));
+        String prodQuantityText = "Quantity: " + String.valueOf(cursor.getInt(prodQuantityIndex));
 
         //set view text data
         prodNameView.setText(prodNameText);
         prodPriceView.setText(prodPriceText);
         prodQuantityView.setText(prodQuantityText);
+
+
+        //bind button click for the sale
+        saleButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //need a method to decrement against the db.
+                //maybe an update where I submit the args and where??
+            }
+        });
     }
 
     /***
